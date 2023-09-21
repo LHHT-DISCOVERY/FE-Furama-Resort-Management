8eeaadcdf14d87724934a0548b888ac7b233f022
LHHT-DISCOVERY <lytri102@gmail.com>
2023-09-21 23:55:49 +0700

    Up task login, authen/author

--- FE-Furama-Resort/src/app/service/token/token-storage.service.ts
+++ FE-Furama-Resort/src/app/service/token/token-storage.service.ts
@@ -4,45 +4,47 @@ const TOKEN_KEY = 'auth-token';
 const USER_KEY = 'auth-user';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TokenStorageService {
 
-  constructor() {
-  }
-
-  public saveToken(token: string) {
-    window.localStorage.removeItem(TOKEN_KEY);
-    window.localStorage.setItem(TOKEN_KEY, token)
-  }
-
-  public saveTokenSession(token: string) {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY, token)
-  }
-
-  public getToken(): string {
-    if (window.localStorage.getItem(TOKEN_KEY) !== null) {
-      return window.localStorage.getItem(TOKEN_KEY);
-    } else {
-      return window.sessionStorage.getItem(TOKEN_KEY);
+    constructor() {
     }
-  }
 
-  public saveUserLocal(user: any) {
-    window.localStorage.removeItem(USER_KEY);
-    window.localStorage.setItem(USER_KEY, JSON.stringify(user));
-  }
-
-  public getUser() {
-    if (window.localStorage.getItem(USER_KEY) !== null) {
-      return JSON.parse(localStorage.getItem(USER_KEY));
-    } else {
-      return JSON.parse(sessionStorage.getItem(USER_KEY));
+    public saveToken(token: string) {
+        window.localStorage.removeItem(TOKEN_KEY);
+        window.localStorage.setItem(TOKEN_KEY, token)
     }
-  }
 
-  public isLogin(): boolean {
-    return !(window.sessionStorage.getItem(TOKEN_KEY) == null && window.localStorage.getItem(USER_KEY) == null)
-  }
+    public saveTokenSession(token: string) {
+        window.sessionStorage.removeItem(TOKEN_KEY);
+        window.sessionStorage.setItem(TOKEN_KEY, token)
+    }
+
+    public getToken(): string {
+        if (localStorage.getItem(TOKEN_KEY) !== null) {
+            //   sử dụng toán tử  || và thêm một chuỗi vào nó để nó không còn null nữa
+            return (localStorage.getItem(TOKEN_KEY) || "");
+        } else {
+            return (sessionStorage.getItem(TOKEN_KEY) || "");
+        }
+    }
+
+    public saveUserLocal(user: any) {
+        window.localStorage.removeItem(USER_KEY);
+        window.localStorage.setItem(USER_KEY, JSON.stringify(user));
+    }
+
+    public getUser() {
+        if (window.localStorage.getItem(USER_KEY) !== null) {
+            //   sử dụng toán tử  || và thêm một chuỗi vào nó để nó không còn null nữa
+            return JSON.parse(localStorage.getItem(USER_KEY) || "");
+        } else {
+            return JSON.parse(sessionStorage.getItem(USER_KEY) || "");
+        }
+    }
+
+    public isLogin(): boolean {
+        return !(window.sessionStorage.getItem(TOKEN_KEY) == null && window.localStorage.getItem(USER_KEY) == null)
+    }
 }
